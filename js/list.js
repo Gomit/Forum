cc77e40f66db35ac963e6bf58466a01214f9e9fd
johansveningsson <Johan@Johans-MacBook-Air.local>
2015-07-18 18:56:52 +0200

    lista av poster

--- js/list.js
+++ js/list.js
@@ -7,13 +7,15 @@ $(document).ready(function(){
 
 function getPosts(){
 	var ref = new Firebase("https://forumforum.firebaseio.com");
-	var postref = ref.child("posts");
+	var postref = ref.child("topics");
 	postref.on("value", function(snapshot) {
+		$('.topics').html("");
 		var data = snapshot.val();
-
+		var row = "";
 		$.each(data, function(key, val){
-			console.log(val.title);
+			row += "<a href='topic_detail.html/"+key+"'><h2>"+val.title+"</h2></a>";
 		});
+		$('.topics').append(row);
 	}, function (errorObject) {
 	  console.log("The read failed: " + errorObject.code);
 	});
